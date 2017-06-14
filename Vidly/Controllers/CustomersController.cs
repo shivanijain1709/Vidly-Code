93e2fbb83008d3297c4f3c298e392d53a443b37f
shivanijain1709 <jainshivani8@gmail.com>
2017-06-14 23:20:46 +0530

    Na
    Na

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ApplicationDbContext _context { get; set; }
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+
+        }
+        public ActionResult New() {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var ViewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm",ViewModel);
+        }
+        [HttpPost]
+        public ActionResult Create(CustomerFormViewModel viewModel) {
+            _context.Customer.Add(viewModel.Customer);
+            _context.SaveChanges();
+            return RedirectToAction("Index","Customers");
+        }
+
+        public ActionResult Edit(int id) {
+
+            var customer = _context.Customer.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                HttpNotFound();
+            var viewModel = new CustomerFormViewModel {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm",viewModel);
+
+        }
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = _context.Customer.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customer.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+        }
+        
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ApplicationDbContext _context { get; set; }
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Genre);
+            return View(movies);
+        }
+        public ActionResult Details(int id) {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            return View(movie);
+        }
+       
+        
+    }
+}
